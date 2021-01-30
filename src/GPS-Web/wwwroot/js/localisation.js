8119f4fc627caaf86f0f0c76a76a1b25d883a4d1
MPD97 <mateuszpracadom@gmail.com>
2021-01-30 15:22:39 +0100

    Form toggler

---
+++ src/GPS-Web/wwwroot/js/localisation.js
@@ -0,0 +1,46 @@
+﻿$(document).ready(function () {
+    console.log("ready!");
+    let formEnabled = false;
+    let name = $("#Name");
+    let deviceName = $("#DeviceName");
+    let highAccuracy = $("#HighAccuracy");
+
+    let button = $("#button");
+
+    button.on("click", function () {
+        var text = $(this).text();
+
+        if (text == "Start") {
+            console.log("Starting");
+            toggleForm();
+        } else if (text == "Stop") {
+            console.log("Stopping");
+            toggleForm();
+        }
+        else {
+            alert("Wrong value of a button!");
+        }
+    });
+
+
+    function toggleForm() {
+        if (formEnabled) {
+            disableForm();
+            formEnabled = false;
+        } else {
+            enableForm();
+            formEnabled = true;
+        }
+        function disableForm() {
+            name.prop("disabled", true);
+            deviceName.prop("disabled", true);
+            highAccuracy.prop("disabled", true);
+        }
+        function enableForm() {
+            name.prop("disabled", false);
+            deviceName.prop("disabled", false);
+            highAccuracy.prop("disabled", false);
+        }
+    }
+
+});
